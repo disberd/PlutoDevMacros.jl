223499307729b99be66a6d111830894182e98405
Alberto Mengali <disberd@gmail.com>
2021-11-27 18:21:58 +0100

    Move mapexr to a notebook

--- notebooks/mapexpr.jl
+++ notebooks/mapexpr.jl
@@ -1,5 +1,14 @@
+### A Pluto.jl notebook ###
+# v0.17.2
+
+# using Markdown
+# using InteractiveUtils
+
+# ╔═╡ ab7fc0f6-d065-4c03-aeb5-64a49214399e
 const default_exprlist = [Symbol("@md_str"), Symbol("@benchmark"), :(using Markdown), :PLUTO_MANIFEST_TOML_CONTENTS, :PLUTO_PROJECT_TOML_CONTENTS]
 
+# ╔═╡ 2ac5f3c6-96f8-4f24-8022-1f0470672383
+#=╠═╡ notebook_exclusive
 """
     hasexpr(expr, exprlist)
 
@@ -20,10 +29,13 @@ function hasexpr(ex, exprlist::AbstractVector)
   end
   return result
 end
+  ╠═╡ notebook_exclusive =#
 
+# ╔═╡ 1663bd40-4a0c-42cd-b716-4583d0a1bb66
 # Fallback for when a single element is provided
 hasexpr(ex,exprlist) = hasexpr(ex,[exprlist])
 
+# ╔═╡ b42062a6-5bca-4c47-835c-caaeee1ceac6
 """
 	include_mapexpr([exprlist])::Function 
 Returns a function that can be used as first argument (`mapexpr`) of [`include`](@ref) calls
@@ -47,3 +59,30 @@ include_mapexpr([3])(:(3 + 2)) == nothing
 ```
 """
 include_mapexpr(exprlist=default_exprlist) = ex -> hasexpr(ex,exprlist) ? nothing : ex
+
+# ╔═╡ b0f0df45-ed36-454a-9a9d-215f8c23836a
+export include_mapexpr
+
+# ╔═╡ 00000000-0000-0000-0000-000000000001
+PLUTO_PROJECT_TOML_CONTENTS = """
+[deps]
+"""
+
+# ╔═╡ 00000000-0000-0000-0000-000000000002
+PLUTO_MANIFEST_TOML_CONTENTS = """
+# This file is machine-generated - editing it directly is not advised
+
+julia_version = "1.7.0-rc2"
+manifest_format = "2.0"
+
+[deps]
+"""
+
+# ╔═╡ Cell order:
+# ╠═ab7fc0f6-d065-4c03-aeb5-64a49214399e
+# ╠═b0f0df45-ed36-454a-9a9d-215f8c23836a
+# ╠═2ac5f3c6-96f8-4f24-8022-1f0470672383
+# ╠═1663bd40-4a0c-42cd-b716-4583d0a1bb66
+# ╠═b42062a6-5bca-4c47-835c-caaeee1ceac6
+# ╟─00000000-0000-0000-0000-000000000001
+# ╟─00000000-0000-0000-0000-000000000002

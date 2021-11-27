f8f13c8b7931ee5d647d7566fca3f469c54e1ae8
Alberto Mengali <disberd@gmail.com>
2021-11-27 18:23:54 +0100

    Added Requires to handle optional load of plutotraits

--- src/PlutoDevMacros.jl
+++ src/PlutoDevMacros.jl
@@ -1,29 +1,21 @@
 module PlutoDevMacros
 
 using MacroTools
-using PlutoHooks: @skip_as_script as @plutohookskip
+using PlutoHooks: @skip_as_script
+using Requires
 
 # This hack is the only way I found to access @skip_as_script as PlutoDevMacros.@skip_as_script
-@eval const $(Symbol("@skip_as_script")) = $(Symbol("@plutohookskip"))
+# @eval const $(Symbol("@skip_as_script")) = $(Symbol("@plutohookskip"))
 
-export @only_in_nb, @only_out_nb, include_mapexpr, @skip_as_script
-export notebook_to_source
+# export @only_in_nb, @only_out_nb, include_mapexpr, @skip_as_script
+# export notebook_to_source
 
-include("mapexpr.jl")
-include(include_mapexpr([default_exprlist..., :InteractiveUtils]),"plutoinclude_macro.jl")
+include("../notebooks/basics.jl") # @only_in_nb, @only_out_nb, is_notebook_local, plutodump, @current_pluto_cell_id, @current_pluto_notebook_file
+include("../notebooks/mapexpr.jl") # hasexpr, default_exprlist, include_mapexpr
+include("../notebooks/plutoinclude_macro.jl") # hasexpr, default_exprlist, include_mapexpr
 
-function is_notebook_local(filesrc)
-	if isdefined(Main,:PlutoRunner)
-		# Try to see if the last 36 character of the source file are a UUID (Pluto cells have their cell_id appended to the file_source)
-		cell_id = tryparse(Base.UUID,last(filesrc,36))
-		cell_id !== nothing && cell_id === Main.PlutoRunner.currently_running_cell_id[] && return true
-	end
-	return false
+function __init__()
+	@require WhereTraits="c9d4e05b-6318-49cb-9b56-e0e2b0ceadd8" include("../notebooks/pluto_traits.jl") # This defines and exports the @plutotraits macro
 end
-
-macro only_in_nb(ex) is_notebook_local(String(__source__.file::Symbol)) ? esc(ex) : nothing end
-macro only_out_nb(ex) is_notebook_local(String(__source__.file::Symbol)) ? nothing : esc(ex) end
-
-
 
 end # module

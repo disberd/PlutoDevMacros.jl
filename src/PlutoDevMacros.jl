d72a3281c249372920dbb9fd153d05d937503266
Alberto Mengali <disberd@gmail.com>
2023-05-05 15:47:11 +0200

    Re-export @fromparent and @frompackage

--- src/PlutoDevMacros.jl
+++ src/PlutoDevMacros.jl
@@ -7,10 +7,12 @@ using HypertextLiteral
 # export @only_in_nb, @only_out_nb, include_mapexpr, @skip_as_script
 # export notebook_to_source
 
-include("../notebooks/basics.jl") # @only_in_nb, @only_out_nb, is_notebook_local, plutodump, @current_pluto_cell_id, @current_pluto_notebook_file
+include("../notebooks/basics.jl") # @only_in_nb, @only_out_nb, is_notebook_local, plutodump, @current_pluto_cell_id, @current_pluto_notebook_file, @addmethod
 module Script include("../notebooks/htlscript.jl") end # HTLScript, HTLBypass, HTLScriptPart, combine_scripts
 
 include("frompackage/FromPackage.jl")
+using .FromPackage
+export @fromparent, @frompackage
 
 include("../notebooks/mapexpr.jl") # hasexpr, default_exprlist, include_mapexpr
 include("../notebooks/plutoinclude_macro.jl") # hasexpr, default_exprlist, include_mapexpr

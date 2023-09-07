55050f13c7554db15f11d691afbf79edad931176
Alberto Mengali <disberd@gmail.com>
2023-09-07 14:00:18 +0200

    move to jsdelivr

--- src/combine_htl/pluto_compat.js
+++ src/combine_htl/pluto_compat.js
@@ -5,8 +5,8 @@ https://github.com/fonsp/Pluto.jl/blob/5b96b85f1a2500dcefb0a739399f77edf5ae78d6/
 but it also adds the _lodash_ package which is available within Pluto
 */
 
-import { Library } from "https://esm.sh/@observablehq/stdlib@3.3.1"
-import {default as lodash} from "https://esm.sh/lodash-es@4.17.20"
+import { Library } from "https://cdn.jsdelivr.net/npm/@observablehq/stdlib@3.3.1/+esm"
+import { default as lodash } from "https://cdn.jsdelivr.net/npm/lodash-es@4.17.20/+esm"
 
 const library = new Library()
 

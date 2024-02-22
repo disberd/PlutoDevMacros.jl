64fa72ccd8036de751edcbda7b86761eb1d07a0d
Alberto Mengali <disberd@gmail.com>
2024-02-22 18:45:52 +0100

    update parse_error notebook

--- test/TestParseError/parseerror_notebook.jl
+++ test/TestParseError/parseerror_notebook.jl
@@ -1,21 +1,21 @@
 ### A Pluto.jl notebook ###
-# v0.19.38
+# v0.19.39
 
 using Markdown
 using InteractiveUtils
 
 # ╔═╡ d4f2f0ca-c463-4318-8fe8-ad41ae9ca998
 begin
-	parent_project = Base.current_project(normpath(@__DIR__, ".."))
+	test_project = Base.current_project(normpath(@__DIR__, ".."))
 	notebook_project = Base.active_project()
 	plutodevmacros_project= Base.current_project(normpath(@__DIR__, "../..")) 
 	Base.eval(Main, quote # instantiate the parent env, mostly for CI
 		import Pkg
-		Pkg.activate($parent_project)
+		Pkg.activate($test_project)
 		Pkg.instantiate()
 		Pkg.activate($notebook_project)
 	end)
-	pushfirst!(LOAD_PATH, parent_project) # This contains Revise
+	pushfirst!(LOAD_PATH, test_project) # This contains Revise
 	pushfirst!(LOAD_PATH, plutodevmacros_project) # This loads the PlutoDevMacros environment, so we can do import with the latest version
 	try
 		Base.eval(Main, :(import Revise))

14237d0be6bc69966be920f262320cbe27bffab2
Alberto Mengali <disberd@gmail.com>
2024-05-27 19:44:43 +0200

    cleanup

--- src/PlutoDevMacros.jl
+++ src/PlutoDevMacros.jl
@@ -1,5 +1,6 @@
 module PlutoDevMacros
 
+using MacroTools: shortdef
 # This are from basics.jl
 export @only_in_nb, @only_out_nb, plutodump, @current_pluto_cell_id,
 @current_pluto_notebook_file, @addmethod
@@ -10,26 +11,5 @@ include("html_helpers.jl")
 include("frompackage/FromPackage.jl")
 using .FromPackage
 export @fromparent, @frompackage
-
-include("../notebooks/mapexpr.jl") # hasexpr, default_exprlist, include_mapexpr
-include("../notebooks/plutoinclude_macro.jl") # hasexpr, default_exprlist, include_mapexpr
-
-# function __init__()
-#     if isdefined(Main, :PlutoRunner)
-#         @info @htl("""
-#         <script>
-#             console.log('This is a script that is ran inside Pluto when loading PlutoDevMacros')
-#             const current_log = currentScript.closest('pluto-log-dot-positioner')
-#             const logs = current_log.parentElement
-#             if (logs.children.length > 1) {
-#                 current_log.style.display = "none"
-#             } else {
-#                 logs.parentElement.style.display = "none"
-#             }
-#         </script>""")
-#     else
-        
-#     end
-# end
 
 end # module

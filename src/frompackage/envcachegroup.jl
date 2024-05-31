edda4392a572973ca805fe583f5e149a2c91b3ef
Alberto Mengali <disberd@gmail.com>
2024-05-31 10:59:51 +0200

    add target as dev in active environment

--- src/frompackage/envcachegroup.jl
+++ src/frompackage/envcachegroup.jl
@@ -1,4 +1,4 @@
-using Pkg.Types: EnvCache, write_project, Context, read_project, read_manifest, write_manifest
+using Pkg.Types: EnvCache, write_project, Context, read_project, read_manifest, write_manifest, PackageSpec, VersionSpec, GitRepo
 
 @kwdef mutable struct EnvCacheGroup
     "This is the EnvCache of the environment added by @fromparent to the LOAD_PATH"
@@ -35,6 +35,16 @@ get_active(ecg::EnvCacheGroup) = ecg.active
 get_target(ecg::EnvCacheGroup) = ecg.target
 get_notebook(ecg::EnvCacheGroup) = ecg.notebook
 
+# This will create a package spec from the target project for adding the target package as `dev` in the active environment
+function target_packagespec(target::EnvCache)
+    (;name, uuid) = target.project
+    path = dirname(target.project_file)
+    repo = GitRepo(;source = path)
+    version = VersionSpec() # We are developing so we put an empty VersionSpec
+    PackageSpec(;name, uuid, version, path, repo)
+end
+
+# This will potentially update the target (or notebook) project the ECG is pointing to
 function maybe_update_envcache(projfile::String, ecg::EnvCacheGroup; notebook = false)
 	f = notebook ? get_notebook : get_target
 	env = f(ecg)
@@ -59,15 +69,16 @@ update_envcache!(::Nothing) = nothing
 function update_ecg!(ecg::EnvCacheGroup; force = false, io::IO = devnull)
 	c = Context(; io)
 	# Update the target and notebook ecg 
-	update_envcache!(ecg |> get_target)
+    target = ecg |> get_target
+	update_envcache!(target)
 	update_envcache!(ecg |> get_notebook)
 	active = get_active(ecg)
 	active_manifest = active |> get_manifest_file
 	active_project = active |> get_project_file
-	target_manifest = get_target(ecg) |> get_manifest_file
+	target_manifest = target |> get_manifest_file
 	if !isfile(target_manifest)
 		@info "It seems that the target package does not have a manifest file. Trying to instantiate its environment"
-		c.env = ecg.target
+		c.env = target
         Pkg.instantiate(c)
 	end
 	if !isfile(active_manifest) || !isfile(active_project)
@@ -75,7 +86,7 @@ function update_ecg!(ecg::EnvCacheGroup; force = false, io::IO = devnull)
 	end
 	if !force
 		active_mtime = mtime(active_manifest)
-		target_mtime = mtime(ecg |> get_target |> get_manifest_file)
+		target_mtime = mtime(target_manifest)
         # Force an update if the target manifest is newer
 		force = force || active_mtime < target_mtime
 	end
@@ -83,7 +94,7 @@ function update_ecg!(ecg::EnvCacheGroup; force = false, io::IO = devnull)
         # This path will update the active Env by copying the project and manifest from the target
 		mkpath(dirname(active_manifest))
 		# We copy a reduced version of the project, only with deps, weakdeps and compat
-        pd = ecg.target.project.other
+        pd = target.project.other
         ad = Dict{String, Any}((k => pd[k] for k in ("deps", "compat", "weakdeps") if haskey(pd, k)))
         write_project(ad, active_project)
         # We copy the Manifest
@@ -91,7 +102,8 @@ function update_ecg!(ecg::EnvCacheGroup; force = false, io::IO = devnull)
         # We call instantiate
 		update_envcache!(ecg.active)
 		c.env = ecg.active
-        Pkg.instantiate(c)
+        # We add the target package as `dev` in the active environment
+        Pkg.develop(c, [target_packagespec(target)])
     end
     return ecg
 end

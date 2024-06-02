0ce274bb49ddda29f00f477da237469a236feaf1
Alberto Mengali <disberd@gmail.com>
2024-06-02 16:14:34 +0200

    fix extension tests

--- test/TestDirectExtension/ext/PlotlyBaseExt.jl
+++ test/TestDirectExtension/ext/PlotlyBaseExt.jl
@@ -1,7 +1,7 @@
 module PlotlyBaseExt
     using PlotlyBase
-    using PlotlyExtensionsHelper
     using TestDirectExtension
+    using TestDirectExtension.PlotlyExtensionsHelper
 
     TestDirectExtension.to_extend(p::Plot) = "Standard Extension works!"
     TestDirectExtension.plot_this() = plotly_plot(rand(10,4))

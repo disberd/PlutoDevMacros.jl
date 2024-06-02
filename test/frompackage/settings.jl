9a17b2e1ffeb6d378e1874244365047c7fcb9797
Alberto Mengali <disberd@gmail.com>
2024-06-02 16:45:01 +0200

    add tests

---
+++ test/frompackage/settings.jl
@@ -0,0 +1,63 @@
+import PlutoDevMacros.FromPackage: FromPackage, add_loadpath, process_settings!, get_setting, get_active, get_project_file
+using Test
+
+TestPackage_path = normpath(@__DIR__, "../TestPackage")
+# We point at the helpers file inside the TestPackage module, we stuff up to the first include
+target = TestPackage_path
+
+# We test that the prefix is there in the temp env
+@test startswith(FromPackage.default_ecg() |> get_active |> get_project_file |> dirname |> basename, "frompackage_")
+
+@test_throws "is not a valid setting" get_setting(:asdfasdf)
+
+@testset "Parsing" begin
+    ex = :(@settings a = 1)
+    # Nothing done without block, as that is anyhow not supported
+    @test ex === process_settings!(ex, Dict())
+    # Test that settings are removed if in a block
+    ex = quote $ex end
+    d = Dict()
+    new_ex = process_settings!(deepcopy(ex), d) |> Base.remove_linenums!
+    @test isempty(new_ex.args)
+    @test haskey(d, "Custom Settings")
+    @test d["Custom Settings"][:a] == 1
+
+    # We check illegal expressions
+    ex = quote
+        @settings a = 1 b = rand()
+    end
+    @test_throws "Only primitive" process_settings!(deepcopy(ex), Dict())
+    ex = quote
+        @settings a = 1 b c
+    end
+    @test_throws "Only `var = value`" process_settings!(deepcopy(ex), Dict())
+
+    # Test the begin end synthax
+    ex = quote
+        @settings begin
+            a = 1
+            b = 2
+        end
+    end
+    d = Dict()
+    new_ex = process_settings!(deepcopy(ex), d) |> Base.remove_linenums!
+    @test isempty(new_ex.args)
+    @test haskey(d, "Custom Settings")
+    @test d["Custom Settings"][:a] == 1
+    @test d["Custom Settings"][:b] == 2
+end
+
+@testset "SHOULD_PREPEND_LOAD_PATH" begin
+    proj_file = FromPackage.default_ecg() |> get_active |> get_project_file
+    l = length(LOAD_PATH)
+    @test proj_file ∉ LOAD_PATH
+    add_loadpath(proj_file; should_prepend = false)
+    @test length(LOAD_PATH) == l+1
+    add_loadpath(proj_file; should_prepend = false)
+    @test length(LOAD_PATH) == l+1
+    @test pop!(LOAD_PATH) == proj_file
+    
+    # Prepend
+    add_loadpath(proj_file; should_prepend = true)
+    @test popfirst!(LOAD_PATH) == proj_file
+end

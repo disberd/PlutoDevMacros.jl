50dd014f0c44d1e7a818c0419b0cbd9feec70a82
Alberto Mengali <disberd@gmail.com>
2025-07-04 10:09:12 +0200

    fix: typo

--- src/frompackage/consts.jl
+++ src/frompackage/consts.jl
@@ -1,5 +1,5 @@
 const IS_DEV = first(fullname(@__MODULE__)) === :Main
-const TEMP_MODULE_NAME = Symbol(:_FrompPackage_TempModule_, IS_DEV ? "DEV_" : "")
+const TEMP_MODULE_NAME = Symbol(:_FromPackage_TempModule_, IS_DEV ? "DEV_" : "")
 const STDLIBS_DATA = Dict{String,Base.UUID}()
 for (uuid, (name, _)) in Pkg.Types.stdlibs()
     STDLIBS_DATA[name] = uuid
